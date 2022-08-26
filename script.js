a4fa2b588fe16128825a889316da7860821068e0
WoiTechs <112165534+WoiTechs@users.noreply.github.com>
2022-08-26 15:37:24 +0100

    added java

---
+++ script.js
@@ -0,0 +1,50 @@
+const clickArea = document.querySelector(".click-area")
+const displayText = document.querySelector(".display-text")
+const scoreElements = document.querySelectorAll(".scores")
+
+const scoreHistory = [];
+
+const MINIMUM_MS_TILL_CHANGE = 3000;
+const MAXIMUM_MS_TILL_CHANGE = 10000;
+
+let msSinceEpochOnTimeout = 0;
+let waitingForClick = false;
+
+function play() {
+    const msTillChange = Math.floor(Math.random() * (MAXIMUM_MS_TILL_CHANGE - MINIMUM_MS_TILL_CHANGE) + MINIMUM_MS_TILL_CHANGE)
+
+    clickArea.style.backgroundColor = null;
+
+    displayText.textContent = "";
+
+    setTimeout(() => {
+        msSinceEpochOnTimeout = Date.now()
+
+        clickArea.style.backgroundColor = "#009578";
+        waitingForClick = true;
+    }, msTillChange);
+
+
+}
+
+function addScores(scores) {
+    scoreHistory.unshift(scores);
+
+    for (let i = 0; i < Math.min(scoreHistory.length, 5); i++) {
+        const score = scoreHistory[i];
+
+        scoreElements[i].textContent = `${score} ms`
+    }
+}
+
+clickArea.addEventListener("click", () => {
+    if (waitingForClick) {
+        const scores =  Date.now() - msSinceEpochOnTimeout;
+
+        waitingForClick = false;
+        displayText.textContent = `Your time was ${scores} ms! Click to play again.`;
+        addScores(scores);
+    } else {
+        play();
+    }
+});
